2066a8f494b2083d4ebf7e943651720471b76125
Branislav Jenco <branislav.jenco@sesam.io>
2021-04-03 16:09:21 +0200

    Reader can read multiple evd files

--- reader.py
+++ reader.py
@@ -2,46 +2,61 @@ import struct
 import traceback
 import argparse
 import os
+import glob
 
 
-total_count = 0
+# the EVD reader and PCL writer classes are adapted from the blensor repository
 
 class EvdReader:
-    rayIndex = 0
-    raysInScan = 0
-    fileHandle = None
+    ray_index = 0
+    rays_in_scan = 0
+    file_handle = None
+    total_count = 0
 
     def __init__(self, filename):
-        self.openEvdFile(filename)
+        self.open_evd_file(filename)
 
-    def openEvdFile(self, filename):
-        self.fileHandle = open(filename, "rb")
+    def open_evd_file(self, filename):
+        self.file_handle = open(filename, "rb")
 
-    def getNextRay(self):
-        global total_count
-        if self.rayIndex >= self.raysInScan:
-            data = struct.unpack("i", self.fileHandle.read(4))
-            self.raysInScan = data[0]
+    def get_next_ray(self):
+        if self.ray_index >= self.rays_in_scan:
+            data = struct.unpack("i", self.file_handle.read(4))
+            self.rays_in_scan = data[0]
             if data[0] == -1:
                 return data[0]
-            self.rayIndex = 0
-        ray = struct.unpack("14dQ", self.fileHandle.read(15 * 8))
-        if total_count < 5:
-            print(ray)
-        self.rayIndex += 1
-        total_count += 1
+            self.ray_index = 0
+        ray = struct.unpack("14dQ", self.file_handle.read(15 * 8))
+        self.ray_index += 1
+        self.total_count += 1
         return ray
 
-    def getRays(self):
+    def get_rays(self):
         count = 0
         while True:
-            ray = self.getNextRay()
+            ray = self.get_next_ray()
             if ray == -1:
                 break
             else:
                 yield ray
             count += 1
         print(f"Finished: {count - 1}")
+
+
+class MultiEvdReader:
+    total_count = 0
+    def __init__(self, filenames):
+        self.filenames = filenames
+
+    def get_rays(self):
+        for filename in self.filenames:
+            reader = EvdReader(filename)
+            for ray in reader.get_rays():
+                yield ray
+                self.total_count += 1
+
+
+
 
 PCL_HEADER = """# .PCD v.7 - Exported by BlenSor
 VERSION .7
@@ -77,7 +92,7 @@ class PCLWriter:
         self.filename = filename
         self.output_labels = output_labels
 
-    def writePCLFile(self, iterator):
+    def write_pcl_file(self, iterator):
         height = 1
 
         try:
@@ -85,7 +100,7 @@ class PCLWriter:
                 count = 0
                 for e in iterator:
                     count += 1
-                    self.writePoint(pcl_tmp, e, self.output_labels)
+                    self.write_point(pcl_tmp, e, self.output_labels)
                 print(f"count {count}")
 
             with open("tmp.pcd", "r") as pcl_tmp:
@@ -102,7 +117,7 @@ class PCLWriter:
         except Exception as e:
             traceback.print_exc()
 
-    def writePoint(self, pcl_noisy, e, output_labels):
+    def write_point(self, pcl_noisy, e, output_labels):
         # Storing color values packed into a single floating point number???
         # That is really required by the pcl library!
         color_uint32 = (int(e[11]) << 16) | (int(e[12]) << 8) | (int(e[13]))
@@ -119,11 +134,21 @@ parser.add_argument(
     '-f',
     help = 'EVD file to read'
 )
+parser.add_argument(
+    '-g',
+    help = 'glob - use a wildcard to specify multiple evd files, these will be made into a single pcl file'
+)
 
 args = parser.parse_args()
 f = args.f
+g = args.g
 
-reader = EvdReader(f)
-writer = PCLWriter(f"{os.path.splitext(f)[0]}")
-writer.writePCLFile(reader.getRays())
+if g:
+    reader = MultiEvdReader(sorted(glob.glob(g)))
+    writer = PCLWriter(f"{os.path.splitext(g)[0]}")
+    writer.write_pcl_file(reader.get_rays())
+elif f:
+    reader = EvdReader(f)
+    writer = PCLWriter(f"{os.path.splitext(f)[0]}")
+    writer.write_pcl_file(reader.get_rays())
 
